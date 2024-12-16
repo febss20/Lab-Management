a755e7a11aadc83482dd099a9bd9cabbaf890b72
febss.rasyid <alifrasyidf@gmail.com>
2024-12-17 00:52:37 +0700

    add contact nip kasublab

--- Lab-Management/server/src/schema/lab.js
+++ Lab-Management/server/src/schema/lab.js
@@ -20,6 +20,8 @@ const labSchema = new mongoose.Schema({
         name: { type: String, required: true },
         email: { type: String, required: true },
         department: { type: String, required: true },
+        contact: { type: String, required: true },
+        nip: { type: String, required: true },
     }, 
 }, { collection: 'lab' })
 
